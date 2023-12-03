ca39580fc65bda33b2b0c5bd4de5053c896bc2af
Steph <36681924+stephherbers@users.noreply.github.com>
2023-12-03 11:55:25 -0500

    add wifi code for remote

--- main.ino
+++ main.ino
@@ -1,7 +1,10 @@
 //author: Steph Herbers
 
 #include "Adafruit_VL53L0X.h"
+#include <AsyncTCP.h>
+#include <AsyncWebServer.h>
 #include <ESP32Servo.h>
+#include "WiFi.h"
 #include <Wire.h>
 
 // Define motor control pins
@@ -19,6 +22,12 @@ const int backLeftDirection = 5;
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 int sda_pin=21, scl_pin=22; //LiDAR pins
+
+const char* ssid = "Tufts_Robot";
+const char* password = "";
+const char* param_servo = "servo";
+const char* param_target = "target";
+AsyncWebServer server(80);
 
 void setup() {
   Serial.begin(9600);
@@ -42,6 +51,54 @@ void setup() {
     Serial.println(F("Failed to boot VL53L0X"));
   while(1);
   }
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Route for root / webpage
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", generateRemoteControlPage());
+  });
+
+  // Route for handling actions from buttons
+  server.on("/control", HTTP_POST, [](AsyncWebServerRequest *request){
+    String command;
+    if (request->hasParam("command", true)) {
+      command = request->getParam("command", true)->value();
+      
+      // Perform action based on the received command
+      if (command == "forward") {
+        // TODO: Code to move forward
+        Serial.println("Moving forward");
+      } else if (command == "backward") {
+        // TODO: Code to move backward
+        Serial.println("Moving backward");
+      } else if (command == "stop") {
+        // TODO: Code to stop movement
+        Serial.println("Stop");
+      }
+    }
+    request->send(200, "text/plain", "OK");
+  });
+
+  // Start server
+  server.begin();
+}
+
+String generateRemoteControlPage() {
+  String remotePage = "<html><body>";
+  remotePage += "<h1>ESP32 Remote Control</h1>";
+  remotePage += "<form action='/control' method='post'>";
+  remotePage += "<button type='submit' name='command' value='forward'>Forward</button>";
+  remotePage += "<button type='submit' name='command' value='backward'>Backward</button>";
+  remotePage += "<button type='submit' name='command' value='stop'>Stop</button>";
+  remotePage += "</form></body></html>";
+  return remotePage;
 }
 
 void controlFrontMotorsSpeed(int speed) {
